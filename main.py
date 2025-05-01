c7fd555cc8a32e86ec38a80358604ee3b0b47301
Christopher Beloch <chris@somekindofcode.com>
2025-05-01 23:04:15 +0200

    add scale parameter

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import inky
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", "-f", type=pathlib.Path, help="Image file")
 parser.add_argument("--rotation", "-r", type=int, help="Screen rotation", default=0)
+parser.add_argument("--scale", "-s", type=str, help="Scale Mode - fill or fit", default="fit")
 
 args, _ = parser.parse_known_args()
 
@@ -34,7 +35,14 @@ if transposing:
     image = image.transpose(transposing) 
 
 # Scale to screen size
-image = image.scale(display.resolution, ResizeMode.SCALE_TO_FIT)
+resizeMode = ResizeMode.SCALE_TO_FIT
+match args.scale:
+    case "fit":
+        resizeMode = ResizeMode.SCALE_TO_FIT
+    case "fill":
+        resizeMode = ResizeMode.SCALE_TO_FILL
+
+image = image.scale(display.resolution, resizeMode)
 
 display.set_image(image, saturation = 0.5)
 display.show()
