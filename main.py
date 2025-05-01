8ab37c52c193dd15e51a6be11b7e5df76c8791fa
Christopher Beloch <chris@somekindofcode.com>
2025-05-01 23:00:24 +0200

    add rotation parameter

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import inky
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", "-f", type=pathlib.Path, help="Image file")
+parser.add_argument("--rotation", "-r", type=int, help="Screen rotation", default=0)
 
 args, _ = parser.parse_known_args()
 
@@ -18,9 +19,22 @@ display = inky.auto()
 # Open Image
 image = Image.open(args.file)
 # Rotate
-image = image.transpose(Transpose.ROTATE_90)
+transposing = None
+match args.rotation:
+    case 90:
+        transposing = Transpose.ROTATE_90
+    case 180:
+        transposing = Transpose.ROTATE_180
+    case -90:
+        transposing = Transpose.ROTATE_270
+    case 270:
+        transposing = Transpose.ROTATE_270
+
+if transposing:
+    image = image.transpose(transposing) 
+
 # Scale to screen size
-image = image.scale(display.resolution, ResizeMode.SCALE_TO_FILL)
+image = image.scale(display.resolution, ResizeMode.SCALE_TO_FIT)
 
 display.set_image(image, saturation = 0.5)
 display.show()
